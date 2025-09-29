f91796543fc75555ce4d5b6756daca8eb15069bf
Ahmed Faragallah <a.faragallah267@gmail.com>
2025-09-29 10:53:44 +0300

    🔄️ refactor(RaMenu) : small refactoring

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -5,27 +5,24 @@ import { styled } from '@mui/material';
 
 export const Menu = () => (
   <StyledRAMenu>
-    <RAMenu.Item to="/" primaryText="البيانات" leftIcon={<DonutLarge />} />
-
-    <RAMenu.ResourceItem name="notes" />
-    <RAMenu.ResourceItem name="reservations" />
-    <RAMenu.ResourceItem name="settings" />
+    <RAMenu.DashboardItem leftIcon={<DonutLarge />} />
+    <RAMenu.ResourceItems />
   </StyledRAMenu>
 );
 
 const StyledRAMenu = styled(RAMenu)(({ theme }) => ({
-  maxWidth: theme.spacing(20),
+  maxWidth: theme.spacing(25),
   fontSize: theme.typography.caption.fontSize,
+
   '& > *': {
     fontSize: 'inherit',
     borderBottom: `2px solid ${theme.palette.divider}`,
+    '&:hover': { borderLeft: `3px solid ${theme.palette.primary.main}` },
   },
   '& .RaMenuItemLink-active': {
     backgroundColor: theme.palette.action.selected,
-    color: theme.palette.primary.main,
+    borderLeft: `3px solid ${theme.palette.primary.main}`,
     fontWeight: theme.typography.fontWeightBold,
-    '& svg': {
-      color: theme.palette.primary.main,
-    },
+    '& .RaMenuItemLink-icon': { color: theme.palette.primary.main },
   },
 }));
