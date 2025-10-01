0ea72ee859e4a93054f6b61b934b01e6dce1e750
Ahmed Faragallah <a.faragallah267@gmail.com>
2025-10-01 12:57:01 +0300

    🔄️ refactor(publication-edit) :  to the new structure

--- src/resources/notes/publications-edit.tsx
+++ src/resources/notes/publications-edit.tsx
@@ -6,69 +6,110 @@ import {
   NumberInput,
   ReferenceInput,
   TextInput,
+  useTranslate,
 } from 'react-admin';
 import { StyledForm } from 'components/form';
 
 import { ImageInput, FileField } from 'react-admin';
 import { AccordionSummary, Accordion, AccordionDetails } from '@mui/material';
 import { KeyboardDoubleArrowDown } from '@mui/icons-material';
+import { Enums } from 'types/supabase-generated.types';
 
-export const PublicationEdit = () => (
-  <Edit>
-    <StyledForm>
-      <ReferenceInput source="subject_id" reference="subjects">
-        <AutocompleteInput
-          sx={{ width: '100%' }}
-          filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
-        />
-        <TextInput sx={{ width: '100%' }} source="nickname" />
-      </ReferenceInput>
-      <ReferenceInput source="teacher_id" reference="teachers">
-        <AutocompleteInput
-          sx={{ width: '100%', fontSize: '1rem' }}
-          filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
-        />
-      </ReferenceInput>
-      <ReferenceInput source="academic_year" reference="academic_years">
-        <AutocompleteInput
-          sx={{ width: '100%' }}
-          filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
-        />
-      </ReferenceInput>
+export const PublicationEdit = () => {
+  const translate = useTranslate();
 
-      <NumberInput sx={{ width: '100%' }} source="pages" />
-      <ImageInput source="cover_url" accept={{ 'image/*': ['.png', '.jpg'] }}>
-        <FileField source="src" title="title" />
-      </ImageInput>
+  const termsOptions = [
+    { id: '1st', name: translate('resources.publications.labels.term.1st') },
+    { id: '2nd', name: translate('resources.publications.labels.term.2nd') },
+    { id: 'full_year', name: translate('resources.publications.labels.term.full_year') },
+  ] as {
+    id: Enums<'term'>;
+    name: string;
+  }[];
+  return (
+    <Edit>
+      <StyledForm>
+        <ReferenceInput source="subject_id" reference="subjects">
+          <AutocompleteInput
+            fullWidth
+            filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
+          />
+        </ReferenceInput>
 
-      <Accordion>
-        <AccordionSummary expandIcon={<KeyboardDoubleArrowDown />} sx={{ width: '100%' }}>
-          المزيد من التفاصيل
-        </AccordionSummary>
-        <AccordionDetails>
-          <TextInput sx={{ width: '100%' }} source="year" />
-          <ReferenceInput source="default_paper_size" reference="paper_types">
+        <ReferenceInput source="publisher" reference="publishers">
+          <AutocompleteInput
+            sx={{ width: '100%', fontSize: '1rem' }}
+            filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
+          />
+        </ReferenceInput>
+
+        <ReferenceInput source="academic_year" reference="academic_years">
+          <AutocompleteInput
+            fullWidth
+            filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
+          />
+        </ReferenceInput>
+
+        <NumberInput fullWidth source="pages" />
+
+        <ImageInput source="cover_url" accept={{ 'image/*': ['.png', '.jpg'] }}>
+          <FileField source="src" title="title" />
+        </ImageInput>
+
+        <Accordion sx={{ '&.Mui-expanded': { m: 0 } }}>
+          <AccordionSummary
+            expandIcon={<KeyboardDoubleArrowDown />}
+            sx={(theme) => ({
+              width: '100%',
+              fontFamily: 'inherit',
+              backgroundColor: theme.palette.action.hover,
+              '& .MuiAccordionSummary-content': {
+                m: 0,
+              },
+              '&.MuiAccordionSummary-root.Mui-expanded': {
+                mb: 2,
+                minHeight: 45,
+              },
+            })}
+          >
+            المزيد من التفاصيل
+          </AccordionSummary>
+
+          <AccordionDetails>
+            <TextInput fullWidth source="year" />
+
+            <ReferenceInput source="default_paper_size" reference="paper_types">
+              <AutocompleteInput
+                fullWidth
+                filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
+              />
+            </ReferenceInput>
+
             <AutocompleteInput
-              sx={{ width: '100%' }}
+              fullWidth
               filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
+              source="term"
+              choices={termsOptions}
             />
-          </ReferenceInput>
-          <ReferenceInput source="term_id" reference="terms">
-            <AutocompleteInput
-              sx={{ width: '100%' }}
-              filterToQuery={(searchText) => ({ 'name@ilike': `%${searchText}%` })}
-            />
-          </ReferenceInput>
-          <TextInput sx={{ width: '100%' }} source="additional_data" />
-          <TextInput sx={{ width: '100%' }} source="related_notes" />
-          <BooleanInput source="do_round" defaultValue={true} />
-          <NumberInput sx={{ width: '100%' }} source="price" />
-          <DateInput source="created_at" sx={{ width: '100%' }} disabled />
-          <TextInput source="created_by" sx={{ width: '100%' }} disabled />
-          <DateInput source="updated_at" sx={{ width: '100%' }} disabled />
-          <TextInput source="updated_by" sx={{ width: '100%' }} disabled />
-        </AccordionDetails>
-      </Accordion>
-    </StyledForm>
-  </Edit>
-);
+
+            <TextInput fullWidth source="additional_data" />
+
+            <TextInput fullWidth source="related_notes" />
+
+            <BooleanInput source="do_round" />
+
+            <NumberInput fullWidth source="price" />
+
+            <DateInput source="created_at" fullWidth disabled />
+
+            <TextInput source="created_by" fullWidth disabled />
+
+            <DateInput source="updated_at" fullWidth disabled />
+
+            <TextInput source="updated_by" fullWidth disabled />
+          </AccordionDetails>
+        </Accordion>
+      </StyledForm>
+    </Edit>
+  );
+};
